2284b3834fd090042e7b9ad16fa726049e4cf3b1
AUCOURT Nicolas <aucourt.nicolas@gmail.com>
2011-03-30 16:55:52 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nixdemoapp::Application.config.secret_token = '91003dfc875f58961a6340803370c8566733d58ff316f560e88b5a40429590ecc50744c0e5f7da9fada544f346415768c9cb555d2469b0d13a5269b5dba643ab'
